be8f8ec7d269cf0dffa8fe098d7d1afa600fc4b8
Teynur Yuseinov <teynur.yuseinov@student.arteveldehs.be>
2024-11-04 20:22:11 +0100

    tablet version fix

--- components/Items/ServicesHome.vue
+++ components/Items/ServicesHome.vue
@@ -1,37 +1,35 @@
 <template>
-    <div class="flex flex-col lg:mt-28 lg:flex-row sm:mt-10 justify-between">
-        <div class="text-maingreen text-center mb-10 flex flex-col items-center">
-            <img src="../../assets/images/wordpress-icon.svg" alt="" class="w-16 sm:w-24 lg:w-32">
-            <div class="border-b-2 pb-2 mt-4 w-40 sm:w-48 lg:w-56 text-xl sm:text-2xl border-maingreen">
+    <div class="flex mt-28 h-lvh justify-between">
+        <div class="text-maingreen text-center w-1/4 flex flex-col items-center">
+            <img src="../../assets/images/wordpress-icon.svg" alt="">
+            <div class="border-b-2 pb-2 mt-4 w-40 text-xl border-maingreen ">
                 <p>WordPress<span class="font-semibold italic"><br>Websites</span></p>
             </div>
-            <div class="w-64 sm:text-2xl sm:w-full lg:w-96 mt-4">
+            <div class=" mt-4">
                 <p>
                     Gebruiksvriendelijk en goed te beheren. Ideaal voor bedrijven die snel online willen met een professionele website.
                 </p>
             </div>
         </div>
-
-        <div class="text-maingreen text-center mb-10 flex flex-col items-center">
-            <img src="../../assets/images/webapp-icon.svg" alt="" class="w-16 sm:w-24 lg:w-32">
-            <div class="border-b-2 pb-2 mt-4 w-40 sm:w-48 lg:w-56 text-xl sm:text-2xl border-maingreen">
-                <p>Moderne<span class="font-semibold italic"><br>Webapplicaties</span></p>
+                <div class="text-maingreen text-center w-1/4 flex flex-col items-center">
+            <img src="../../assets/images/webapp-icon.svg" alt="">
+            <div class="border-b-2 pb-2 mt-4 w-40 text-xl border-maingreen ">
+                <p>Moderne<span class="font-semibold italic"><br>Websites</span></p>
             </div>
-            <div class="w-64 sm:text-2xl sm:w-full lg:w-96 mt-4">
+            <div class=" mt-4">
                 <p>
-                    Op maat gemaakte websites met moderne frameworks. Deze zijn supersnel, schaalbaar, en bieden geavanceerde mogelijkheden voor unieke, dynamische webervaringen.
+                    Gebruiksvriendelijk en goed te beheren. Ideaal voor bedrijven die snel online willen met een professionele website.
                 </p>
             </div>
         </div>
-
-        <div class="text-maingreen text-center flex flex-col items-center">
-            <img src="../../assets/images/visual-icon.svg" alt="" class="w-16 sm:w-24 lg:w-32">
-            <div class="border-b-2 pb-2 mt-4 w-40 sm:w-48 lg:w-56 text-xl sm:text-2xl border-maingreen">
-                <p>Visuele<span class="font-semibold italic"><br>Identiteit</span></p>
+                <div class="text-maingreen text-center w-1/4 flex flex-col items-center">
+            <img src="../../assets/images/visual-icon.svg" alt="">
+            <div class="border-b-2 pb-2 mt-4 w-40 text-xl border-maingreen ">
+                <p>Visuele<span class="font-semibold italic"><br>identiteit</span></p>
             </div>
-            <div class="w-64 sm:text-2xl sm:w-full mb-10 lg:w-96 mt-4">
-                <p>
-                    Sterke visuele identiteiten die jouw merk krachtig en duurzaam neerzetten, van logo-ontwerp tot complete huisstijl en branding.
+            <div class="mt-4">
+                <p class="">
+                    Gebruiksvriendelijk en goed te beheren. Ideaal voor bedrijven die snel online willen met een professionele website.
                 </p>
             </div>
         </div>
